35e13985c946a52f499d005f91ce08c5d28e24db
Deeksha <deekshad1502@gmail.com>
2024-07-19 12:57:25 +0530

    feat/header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
 import logo from '../Header/logo.png';
 import { RxHamburgerMenu } from 'react-icons/rx';
 import { IoIosSearch } from 'react-icons/io';
+import { MdOutlineRefresh } from 'react-icons/md';
+import { FaListUl } from 'react-icons/fa';
+import { IoSettingsOutline } from 'react-icons/io5';
 
 export const Header = () => {
   return (
@@ -16,13 +19,23 @@ export const Header = () => {
         <div className='relative md:ml-24'>
           <div className='flex-grow'></div>
           <input
-            className='block w-60 md:w-[700px] p-4 ps-10 mx-auto text-gray-900 border border-gray-300 rounded-lg bg-gray-50  outline-none text-md '
+            className='block w-60 md:w-[900px] p-4 ps-10 mx-auto text-gray-900 border border-gray-300 rounded-lg bg-gray-50  outline-none text-md '
             placeholder='Search..'
             type='text'
           />
           <IoIosSearch className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 size-6' />
         </div>
-        <div className='flex-grow'></div>
+        <div className='flex justify-between gap-8 mx-5  items-center'>
+          <div>
+            <MdOutlineRefresh className='size-7 text-gray-500' />
+          </div>
+          <div>
+            <FaListUl className='size-7 text-gray-500' />
+          </div>
+          <div>
+            <IoSettingsOutline className='size-7 text-gray-500' />
+          </div>
+        </div>
       </div>
       <hr className='h-px my-3 bg-gray-200 border-0 dark:bg-gray-700'></hr>
     </div>
