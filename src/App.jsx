4c876d50f91fbe6ad7a44c8df8fb508c6f69028d
Deeksha <deekshad1502@gmail.com>
2024-07-21 22:12:36 +0530

    feat/add text to editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
-import { Body } from './components/Body/Body';
+// import { Body } from './components/Body/Body';
+import { Editor } from './components/Editor/Editor';
 import { Header } from './components/Header/Header';
+import { v4 as uuidV4 } from 'uuid';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 export const App = () => {
   return (
-    <>
+    <Router>
       <div>
         <Header />
-        <Body />
+        {/* <Body /> */}
+        <Routes>
+          <Route path='/' element={<Navigate to={`/documents/${uuidV4()}`} replace />} />
+
+          <Route path='/documents/:id' element={<Editor />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 };
